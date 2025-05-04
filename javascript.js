928a85ccc0cce8db078b4c75b1879daabf653de0
hpeter19 <njoshpitar@gmail.com>
2025-05-04 20:15:16 +0300

    adding function on  a button

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let a=10;
+
+a=13;
+console.log(a)
+
+a=13;
+
+const button = document.querySelector("button");
+
+function greet() {
+  const name = prompt("What is your name?");
+  const greeting = document.querySelector("#greeting");
+  greeting.textContent = `Hello ${name}, nice to see you!`;
+}
+
+button.addEventListener("click", greet);
